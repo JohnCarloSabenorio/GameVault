9dc706c326e0be72b04fc89c532963ee62a5bf9e
Bluem <140798194+JohnCarloSabenorio@users.noreply.github.com>
2025-10-13 15:01:22 +0800

    updated ms sql server tables

--- server/obj/Debug/net9.0/server.AssemblyInfo.cs
+++ server/obj/Debug/net9.0/server.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5f103437f45329762a38d3f46e82a0c959235d68")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0b232d3c69d8eebc85c5bb6bbe39035c52e14d26")]
 [assembly: System.Reflection.AssemblyProductAttribute("server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
