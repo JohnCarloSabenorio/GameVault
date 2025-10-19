7667dafea12b7f34880e1354c96f9789327ff269
Bluem <140798194+JohnCarloSabenorio@users.noreply.github.com>
2025-10-19 21:11:06 +0800

    commit

--- server/obj/Debug/net9.0/server.AssemblyInfo.cs
+++ server/obj/Debug/net9.0/server.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2d860cf28198aa6ef6ca7c0e4e7307c119feeb1e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0bc7105c4558bea704d1365e4a9b9e319ce2405f")]
 [assembly: System.Reflection.AssemblyProductAttribute("server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
