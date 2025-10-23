c86a1d7eda167b19acf15747f8dc16fb3c12e606
Bluem <140798194+JohnCarloSabenorio@users.noreply.github.com>
2025-10-23 17:16:30 +0800

    add self-referential id for video game dlc

--- server/Models/VideoGame.cs
+++ server/Models/VideoGame.cs
@@ -20,13 +20,16 @@ public class VideoGame
     public long? FranchiseId { get; set; }
     public long? StatusId { get; set; }
     public long? ImageId { get; set; }
+    public long? ParentGameId { get; set; }
     public DateTime ReleaseDate { get; set; }
     public DateTime CreatedAt { get; set; } = DateTime.Now;
     public DateTime UpdatedAt { get; set; } = DateTime.Now;
     public Franchise Franchise { get; set; }
     public Status Status { get; set; }
     public Image Image { get; set; }
+    public VideoGame ParentGame { get; set; }
 
+    public List<VideoGame> DlCList { get; set; } = new List<VideoGame>();
     public List<Review> Reviews { get; set; } = new List<Review>();
     public List<VideoGameGenre> VideoGameGenres { get; set; } = new List<VideoGameGenre>();
 }
