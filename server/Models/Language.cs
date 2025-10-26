219513ce11735b385283457059e5a694c56ce0fe
Bluem <140798194+JohnCarloSabenorio@users.noreply.github.com>
2025-10-26 16:53:11 +0800

    added language and video model

---
+++ server/Models/Language.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace server.Models
+{
+    public class Language
+    {
+        public long Id { get; set; }
+        public string? Locale { get; set; }
+        public string? Name { get; set; }
+        public string? NativeName { get; set; }
+    }
+}
