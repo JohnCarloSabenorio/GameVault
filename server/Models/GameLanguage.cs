4eebd6f2ec81fe16ca23c2efbffe5edc428f3845
Bluem <140798194+JohnCarloSabenorio@users.noreply.github.com>
2025-10-29 00:16:02 +0800

    added game language model

---
+++ server/Models/GameLanguage.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace server.Models
+{
+    public class GameLanguage
+    {
+        public long GameId { get; set; }
+        public long LanguageId { get; set; }
+        public DateTime CreatedAt { get; set; } = DateTime.Now;
+        public DateTime UpdatedAt { get; set; } = DateTime.Now;
+        public Game? Game { get; set; }
+        public Language? Language { get; set; }
+    }
+}
