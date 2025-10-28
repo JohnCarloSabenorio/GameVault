011ad4ea4b4ec6688b4c361cf34dcf02e155c817
Bluem <140798194+JohnCarloSabenorio@users.noreply.github.com>
2025-10-28 18:52:39 +0800

    added game collection dto and mapper

---
+++ server/DTOs/GameCollection/GameCollectionDTO.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace server.DTOs.GameCollection
+{
+    public class GameCollectionDTO
+    {
+        public long GameId { get; set; }
+        public long CollectionId { get; set; }
+    }
+}
---
+++ server/Mappers/GameCollectionMapper.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using server.DTOs.GameCollection;
+using server.Models;
+
+namespace server.Mappers
+{
+    public static class GameCollectionMapper
+    {
+        public static GameCollectionDTO GameCollectionDTO(GameCollection gameCollection)
+        {
+            return new GameCollectionDTO { GameId = gameCollection.GameId, CollectionId = gameCollection.CollectionId };
+        }
+    }
+}
